4e6ea04490d83fe90d732738b231d7b6d428c526
vani_v <kathirojuvani@gmail.com>
2024-04-23 15:37:24 +0530

    assignment4
    commit it

--- src/miniprojects/test_Assignment4.py
+++ src/miniprojects/test_Assignment4.py
@@ -5,9 +5,48 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from allure_commons.types import AttachmentType
 @pytest.mark.smoke
-@allure.title("Verify that codepen registered page is opened ")
-@allure.description("TC#1 - simple enter all email,password,confirm password and click submit button.")
+@allure.title("Login page of ebay website ")
+@allure.description("Give me the cheapest one from the list.")
+@allure.label("owner", "vani valaboju")
+@allure.link("https://www.ebay.com/", name="Website")
+@allure.testcase("TC-1")
+
 def test_open_ebay():
         driver = webdriver.Chrome()
         driver.get("https://www.ebay.com/")
+        driver.maximize_window()
+        time.sleep(5)
+        assert driver.current_url == "https://www.ebay.com/"
+        allure.attach(driver.get_screenshot_as_png(), name='Home_Page_Screenshot', attachment_type=AttachmentType.PNG)
+
+        search_element = driver.find_element(By.XPATH, "//input[@id='gh-ac']")
+        search_element.send_keys("16gb")
+
+        search_btn_element = driver.find_element(By.XPATH, "//input[@id='gh-btn']")
+        search_btn_element.click()
+        allure.attach(driver.get_screenshot_as_png(), name='Product_Page_Screenshot',
+                      attachment_type=AttachmentType.PNG)
+
+        time.sleep(5)
+        list_of_elements = driver.find_elements(By.XPATH, "//span[@role='heading']")
+
+        for product in list_of_elements:
+                product_name = product.text
+                print(product_name)
+
+        product_price_element_list = driver.find_elements(By.XPATH, "//span[@class='s-item__price']")
+        prize = []
+        for price in product_price_element_list:
+                product_price = price.text
+                print(product_price)
+                x = product_price.replace("$", " ").strip()
+                prize.append(x)
+
+        prize.sort()
+
+        # min_price = prize[1]
+        print(f"cheapest price is {prize[1]}")
+        allure.attach(driver.get_screenshot_as_png(), name='cheapest_price_Page_Screenshot',
+                      attachment_type=AttachmentType.PNG)
+
         time.sleep(5)
