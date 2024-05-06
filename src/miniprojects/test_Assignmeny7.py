e700a4cba0be28516bd0b1a2c4c6d81a9d59e544
vani_v <kathirojuvani@gmail.com>
2024-05-06 21:39:39 +0530

    Update test_Assignmeny7.py
    submit

--- src/miniprojects/test_Assignmeny7.py
+++ src/miniprojects/test_Assignmeny7.py
@@ -1,4 +1,4 @@
-mport pytest
+import pytest
 import allure
 import time
 from selenium import webdriver
@@ -9,25 +9,18 @@ from selenium.webdriver.support.relative_locator import locate_with
 
 @pytest.mark.smoke
 @allure.title("Verify that codepen registered page is opened ")
-@allure.description("TC#1 - simple enter all email,password,confirm password and click submit button.")
-def test_open_CodePen():
+@allure.description("TC#1 - Below element - error message assert that error message.")
+def test_MiniAssignment7():
     driver = webdriver.Chrome()
     driver.get("https://codepen.io/AbdullahSajjad/full/LYGVRgK")
     time.sleep(5)
     driver.switch_to.frame(driver.find_element(By.ID, "result"))
-    element_email = driver.find_element(locate_with(By.XPATH,"//input[@id='email']").below('username')
-    element_email.send_keys("vani.v3509@gmail.com")
+    button = driver.find_element(By.XPATH, "//button[text()='Submit']")
+    button.click()
     time.sleep(3)
-    element_password = driver.find_element(By.XPATH,"//input[@id='password']")
-    element_password.send_keys(123456)
-    time.sleep(3)
-    element_againpass=driver.find_element(By.XPATH,"//input[@id='password2']")
-    element_againpass.send_keys(123456)
-    time.sleep(3)
-    submit_btn = driver.find_element(By.XPATH, value="//button[text()='Submit']")
-    submit_btn.click()
-    time.sleep(3)
+    user_name = driver.find_element(By.XPATH,"//input[@id='username']")
+    err_msg = driver.find_element(locate_with(By.TAG_NAME,"small").below(user_name)).text
+    assert err_msg == "Username must be at least 3 characters"
     allure.attach(driver.get_screenshot_as_png(), name="login-screenshot", attachment_type=AttachmentType.PNG)
-    assert (driver.find_element(By.XPATH,"//input[@id='username']/following::small").text
-            == "Username must be at least 3 characters")
+    time.sleep(5)
     driver.quit()
